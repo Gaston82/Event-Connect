a9cf3d77857590128cdd201841eb86c974b471f9
Xavi Murcia <xavier.murcia@wefoxgroup.com>
2020-04-08 23:44:02 +0200

    Fix inifinite network calls

--- src/components/listEvents/ListEvents.js
+++ src/components/listEvents/ListEvents.js
@@ -10,66 +10,51 @@ const ListEvents = () => {
   const [eventList, setEventList] = useState([]);
   const [keyword, setKeyword] = useState('');
 
- 
+
   useEffect(() => {
-    if(keyword){
-      getArtist(keyword);
+    const getArtist = async () => {
+      const apikey = `DxSOpYSZ4nVwPWsGOWdELH14DJA5EIYL`;
+      const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apikey}&countryCode=ES&keyword=${keyword}`;
+      const response = await axios(url);
+
+      setEventList(response.data._embedded.events)
     }
+
+    getArtist(keyword);
   }, [keyword]);
 
+  // Esto se necesita? Cuando haces la llamada sin keyword o con la keyword siendo string vacia hace el mismo efecto
+  // useEffect(() => {
+  //   const getEvents = async () => {
+  //     const apikey = `DxSOpYSZ4nVwPWsGOWdELH14DJA5EIYL`;
+  //     const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apikey}&countryCode=ES`;
+  //     const response = await axios(url);
+  //     console.log(response.data._embedded.events);
 
+  //     setEventList(response.data._embedded.events);
+  //   };
 
+  //   getEvents();
 
-  const getArtist = async (keyword) => {
-    const apikey = `DxSOpYSZ4nVwPWsGOWdELH14DJA5EIYL`;
-    const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apikey}&countryCode=ES&keyword=${keyword}`;
-    const response = await axios(url);
-    
-    setEventList(response.data._embedded.events)
-  }
- 
-
-  const getEvents = async () => {
-    const apikey = `DxSOpYSZ4nVwPWsGOWdELH14DJA5EIYL`;
-    const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apikey}&countryCode=ES`;
-    const response = await axios(url);
-    console.log(response.data._embedded.events);
-    
-
-    setEventList(response.data._embedded.events);
-  };
-
-  useEffect(() => {
-    if(!keyword)
-    getEvents();
-    
-  }, [eventList]);
-
-
+  // }, []);
 
   return(
-      <>
-      <div className="header">
-          <Header/> 
-          <User />
-          <SearchEvent 
-          setKeyword={setKeyword}
-          />
-      </div>
-      <div className="contenedor-listEvents">
-              {eventList.map((event) =>(
-          <div className="event-item">
-                  <Event 
-                  key={event.id}
-                  event={event}
-                  />
-                  </div>
-              ))}
-          
-      </div>
-  )
-     
-     </>
+    <>
+    <div className="header">
+      <Header/>
+      <User />
+      <SearchEvent
+      setKeyword={setKeyword}
+      />
+    </div>
+    <div className="contenedor-listEvents">
+      {eventList.map((event) => (
+      <div className="event-item" key={event.id}>
+              <Event event={event}/>
+              </div>
+          ))}
+    </div>
+    </>
   )};
 
 export default ListEvents;
